80c8c8205fda2ebdabfc805962735bda5d46f602
JosiahL06 <114594514+JosiahL06@users.noreply.github.com>
2026-03-28 15:20:19 -0400

    Update ArduinoControlCode.ino

--- FunctionalPrototype/ArduinoControlCode.ino
+++ FunctionalPrototype/ArduinoControlCode.ino
@@ -3,14 +3,15 @@
 // https://github.com/JosiahL06/SeniorDesignPrototype/blob/main/FunctionalPrototype/ArduinoControlCode.ino
 // Last Updated: 3/29 by Josiah Laakkonen
 // TODO:
-//        - (in progress) Add BLE security encryption
+//        - Add BLE security encryption
 //        - Define commands to allow motor testing
 //        - Define commands to allow BLE testing
 // =============================
-// During Runtime:
+// LED logic during runtime:
 //        - LED blinks blue when searching for connection
 //        - LED turns green when connected
 //        - LED blinks red when a command is running
+// =============================
 
 #include <BLEDevice.h>
 #include <BLEServer.h>
@@ -21,8 +22,8 @@
 // =============================
 // BLE Configuration
 // =============================
-#define DEVICE_NAME         "NanoESP32-Willow-BLE"
-#define SERVICE_UUID        "55f8a5ee-886f-4929-a3ab-5745cbbceab5"
+#define DEVICE_NAME "NanoESP32-Willow-BLE"
+#define SERVICE_UUID "55f8a5ee-886f-4929-a3ab-5745cbbceab5"
 #define CHARACTERISTIC_UUID "a6a06cf5-71b2-489b-9f03-84dfe6fc6330"
 
 // =============================
@@ -37,53 +38,52 @@ bool deviceConnected = false;
 // Server Callbacks
 // =============================
 class ServerCallbacks : public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-      Serial.println("BLE client connected");
-    }
+  void onConnect(BLEServer *pServer) {
+    deviceConnected = true;
+    Serial.println("BLE client connected");
+  }
 
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-      Serial.println("BLE client disconnected");
-      pServer->startAdvertising();  // Restart advertising
-    }
+  void onDisconnect(BLEServer *pServer) {
+    deviceConnected = false;
+    Serial.println("BLE client disconnected");
+    pServer->startAdvertising();  // Restart advertising
+  }
 };
 
 // =============================
 // Command Callbacks
 // =============================
 class CommandCallbacks : public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic* pCharacteristic) {
-      std::string value = pCharacteristic->getValue();
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    std::string value = pCharacteristic->getValue();
 
-      if (value.empty()) return;
+    if (value.empty()) return;
 
-      Serial.print("Received command: ");
-      Serial.println(value.c_str());
+    Serial.print("Received command: ");
+    Serial.println(value.c_str());
 
-      // =============================
-      // Commands
-      // =============================
+    // =============================
+    // Commands
+    // =============================
 
-      // ON
-      if (value == "ON") {
-        digitalWrite(LED_PIN, HIGH);
-        Serial.println("Pin ON");
-      }
-      // OFF
-      else if (value == "OFF") {
-        digitalWrite(LED_PIN, LOW);
-        Serial.println("Pin OFF");
-      }
-      // TOGGLE
-      else if (value == "TOGGLE") {
-        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-        Serial.println("Pin TOGGLED");
-      }
-      else {
-        Serial.println("Unknown command");
-      }
+    // ON
+    if (value == "ON") {
+      digitalWrite(LED_PIN, HIGH);
+      Serial.println("Pin ON");
     }
+    // OFF
+    else if (value == "OFF") {
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("Pin OFF");
+    }
+    // TOGGLE
+    else if (value == "TOGGLE") {
+      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+      Serial.println("Pin TOGGLED");
+    } else {
+      Serial.println("Unknown command");
+    }
+  }
 };
 
 
@@ -97,13 +97,13 @@ void setup() {
   Serial.println("Starting BLE device...");
 
   // Initialize BLE
-  BLEDevice::init("NanoESP32-Willow-BLE");
+  BLEDevice::init(DEVICE_NAME);
 
-  // Enable BLE Security
-  BLESecurity *pSecurity = new BLESecurity();
-  pSecurity->setAuthenticationMode(ESP_LE_AUTH_REQ_SC_ONLY);
-  pSecurity->setCapability(ESP_IO_CAP_NONE);
-  pSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
+  // Enable BLE Security (currently not working)
+  //BLESecurity *pSecurity = new BLESecurity();
+  //pSecurity->setAuthenticationMode(ESP_LE_AUTH_REQ_SC_ONLY);
+  //pSecurity->setCapability(ESP_IO_CAP_NONE);
+  //pSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
 
   // Create BLE Server and set server callbacks
   BLEServer *pServer = BLEDevice::createServer();
@@ -114,11 +114,14 @@ void setup() {
 
   // Create BLE Characteristic
   pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_WRITE
-                    );
-  
-  pCharacteristic->setAccessPermissions(ESP_GATT_PERM_WRITE_ENCRYPTED);
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_WRITE |
+    BLECharacteristic::PROPERTY_NOTIFY
+  );
+
+  //pCharacteristic->setAccessPermissions(
+  //  ESP_GATT_PERM_WRITE_ENCRYPTED
+  //);
 
   // Set command callbacks
   pCharacteristic->setCallbacks(new CommandCallbacks());
